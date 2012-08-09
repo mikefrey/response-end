5045766f045d33523572fea30cd1f3efe9e5d668
Mike Frey <mike.frey@tstmedia.com>
2012-08-08 19:47:56 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * response-end
+ *
+ * Add an 'end' event to the http server response
+ *
+ * © 2012 Mike Frey <frey.mike@gmail.com>
+ * MIT Licensed
+ */
+
+module.exports = responseEnd
+
+function responseEnd(res) {
+  var old = res.end
+  res.end = function() {
+    res.end = old
+    res.end.apply(res, arguments)
+    res.emit('end')
+  }
+}
