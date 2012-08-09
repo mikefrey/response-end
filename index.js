5b8248cd32c100d559b940d8ce6684dabaa582e4
Mike Frey <mike.frey@tstmedia.com>
2012-08-08 21:50:11 -0500

    added proxied property to prevent proxying the method multiple times

--- index.js
+++ index.js
@@ -10,10 +10,12 @@
 module.exports = responseEnd
 
 function responseEnd(res) {
+  if (res.end.proxied) return
   var old = res.end
   res.end = function() {
     res.end = old
     res.end.apply(res, arguments)
     res.emit('end')
   }
+  res.end.proxied = true
 }
